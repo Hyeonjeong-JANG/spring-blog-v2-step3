b9f9801a6a3dba8887d941aa950db5aa943a0318
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-03-14 20:20:53 +0900

    게시글 상세보기

---
+++ src/test/java/shop/mtcoding/blog/board/BoardRepositoryTest.java
@@ -0,0 +1,36 @@
+package shop.mtcoding.blog.board;
+
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+@Import(BoardRepository.class)
+@DataJpaTest
+public class BoardRepositoryTest {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Autowired
+    private EntityManager em;
+
+
+    @Test
+    public void findById_test() {
+        // given
+        int id = 1;
+
+        // when
+        System.out.println("start - 1");
+        Board board = boardRepository.findById(id); // 1차 셀렉트 쿼리 발동
+
+        // then
+        System.out.println("start - 2");
+        System.out.println(board.getUser().getId());
+        System.out.println("start - 3");
+        System.out.println(board.getUser().getUsername());
+    }
+}
